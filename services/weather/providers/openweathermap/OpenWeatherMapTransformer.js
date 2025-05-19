cfb202fa3f99a1841471ca185d4910a9828781c5
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2025-05-19 09:45:17 +0700

    Flatten normalized data

--- services/weather/providers/openweathermap/OpenWeatherMapTransformer.js
+++ services/weather/providers/openweathermap/OpenWeatherMapTransformer.js
@@ -10,7 +10,6 @@ class OpenWeatherMapTransformer extends BaseTransformer {
 
     validate(data) {
         if (data.list) {
-            // Forecast data validation
             return data && 
                 data.list && 
                 data.list.length > 0 &&
@@ -18,7 +17,6 @@ class OpenWeatherMapTransformer extends BaseTransformer {
                 data.list[0].main &&
                 data.list[0].wind;
         }
-        // Current weather validation
         return data && 
             (data.weather || data.list) && 
             data.main && 
@@ -27,63 +25,63 @@ class OpenWeatherMapTransformer extends BaseTransformer {
 
     _transformCurrent(data) {
         return {
-            location: {
-                city: data.name,
-                country: data.sys.country,
-                coordinates: {
-                    lat: data.coord.lat,
-                    lon: data.coord.lon
-                }
-            },
-            temperature: {
-                current: data.main.temp,
-                feelsLike: data.main.feels_like,
-                min: data.main.temp_min,
-                max: data.main.temp_max
-            },
+            // Location data
+            city: data.name,
+            country: data.sys.country,
+            latitude: data.coord.lat,
+            longitude: data.coord.lon,
+            
+            // Temperature data
+            temperature: data.main.temp,
+            feelsLike: data.main.feels_like,
+            tempMin: data.main.temp_min,
+            tempMax: data.main.temp_max,
+            
+            // Atmospheric conditions
             humidity: data.main.humidity,
             pressure: data.main.pressure,
-            wind: {
-                speed: data.wind.speed,
-                direction: data.wind.deg
-            },
-            conditions: {
-                main: data.weather[0].main,
-                description: data.weather[0].description,
-                icon: data.weather[0].icon
-            },
+            
+            // Wind data
+            windSpeed: data.wind.speed,
+            windDirection: data.wind.deg,
+            
+            // Weather conditions
+            conditionMain: data.weather[0].main,
+            conditionDescription: data.weather[0].description,
+            conditionIcon: data.weather[0].icon,
+            
+            // Timestamp
             timestamp: new Date(data.dt * 1000)
         };
     }
 
     _transformForecast(data) {
         return {
-            location: {
-                city: data.city.name,
-                country: data.city.country,
-                coordinates: {
-                    lat: data.city.coord.lat,
-                    lon: data.city.coord.lon
-                }
-            },
+            city: data.city.name,
+            country: data.city.country,
+            latitude: data.city.coord.lat,
+            longitude: data.city.coord.lon,
             forecast: data.list.map(item => ({
-                temperature: {
-                    current: item.main.temp,
-                    feelsLike: item.main.feels_like,
-                    min: item.main.temp_min,
-                    max: item.main.temp_max
-                },
+                // Temperature data
+                temperature: item.main.temp,
+                feelsLike: item.main.feels_like,
+                tempMin: item.main.temp_min,
+                tempMax: item.main.temp_max,
+                
+                // Atmospheric conditions
                 humidity: item.main.humidity,
                 pressure: item.main.pressure,
-                wind: {
-                    speed: item.wind.speed,
-                    direction: item.wind.deg
-                },
-                conditions: {
-                    main: item.weather[0].main,
-                    description: item.weather[0].description,
-                    icon: item.weather[0].icon
-                },
+                
+                // Wind data
+                windSpeed: item.wind.speed,
+                windDirection: item.wind.deg,
+                
+                // Weather conditions
+                conditionMain: item.weather[0].main,
+                conditionDescription: item.weather[0].description,
+                conditionIcon: item.weather[0].icon,
+                
+                // Timestamp
                 timestamp: new Date(item.dt * 1000)
             }))
         };
