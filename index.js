e31df492015d78be4146942afbf0ecea787dd3ef
hoangquochung1110 <arsenalforeversince2007@gmail.com>
2025-05-21 16:07:26 +0700

    Configure CORS headers

--- index.js
+++ index.js
@@ -16,8 +16,22 @@ class Application extends EventEmitter {
   }
 
   initializeMiddlewares() {
+    // Enable CORS for all routes
+    this.app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      
+      // Handle preflight requests
+      if (req.method === 'OPTIONS') {
+        return res.sendStatus(200);
+      }
+      
+      next();
+    });
+    
     this.app.use(express.json());
-    // Add other middlewares here (cors, helmet, etc.)
+    // Add other middlewares here (helmet, etc.)
   }
 
 
